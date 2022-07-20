4a816236f9ac4284ffce81196106dc008849b94c
alona <alona@LAPTOP-QTECAFMK>
2022-07-20 15:54:51 -0400

    Add project files.

---
+++ RythmicQuebecInventory/GUI/FormViewSearch.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace RythmicQuebecInventory
+{
+    partial class FormViewSearch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxFilterElements = new System.Windows.Forms.GroupBox();
+            this.labelCategory = new System.Windows.Forms.Label();
+            this.labelSize = new System.Windows.Forms.Label();
+            this.labelColor = new System.Windows.Forms.Label();
+            this.labelBoxNo = new System.Windows.Forms.Label();
+            this.labelQuantity = new System.Windows.Forms.Label();
+            this.labelViewAndSearchWindow = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonAdminAccess = new System.Windows.Forms.Button();
+            this.groupBoxResults = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBoxFilterElements.SuspendLayout();
+            this.groupBoxResults.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBoxFilterElements
+            // 
+            this.groupBoxFilterElements.Controls.Add(this.labelQuantity);
+            this.groupBoxFilterElements.Controls.Add(this.labelBoxNo);
+            this.groupBoxFilterElements.Controls.Add(this.labelColor);
+            this.groupBoxFilterElements.Controls.Add(this.labelSize);
+            this.groupBoxFilterElements.Controls.Add(this.labelCategory);
+            this.groupBoxFilterElements.Location = new System.Drawing.Point(0, 79);
+            this.groupBoxFilterElements.Name = "groupBoxFilterElements";
+            this.groupBoxFilterElements.Size = new System.Drawing.Size(300, 955);
+            this.groupBoxFilterElements.TabIndex = 0;
+            this.groupBoxFilterElements.TabStop = false;
+            this.groupBoxFilterElements.Text = "Filter Elements";
+            // 
+            // labelCategory
+            // 
+            this.labelCategory.AutoSize = true;
+            this.labelCategory.Location = new System.Drawing.Point(31, 22);
+            this.labelCategory.Name = "labelCategory";
+            this.labelCategory.Size = new System.Drawing.Size(82, 16);
+            this.labelCategory.TabIndex = 0;
+            this.labelCategory.Text = "CATEGORY";
+            // 
+            // labelSize
+            // 
+            this.labelSize.AutoSize = true;
+            this.labelSize.Location = new System.Drawing.Point(31, 144);
+            this.labelSize.Name = "labelSize";
+            this.labelSize.Size = new System.Drawing.Size(36, 16);
+            this.labelSize.TabIndex = 1;
+            this.labelSize.Text = "SIZE";
+            // 
+            // labelColor
+            // 
+            this.labelColor.AutoSize = true;
+            this.labelColor.Location = new System.Drawing.Point(31, 304);
+            this.labelColor.Name = "labelColor";
+            this.labelColor.Size = new System.Drawing.Size(53, 16);
+            this.labelColor.TabIndex = 2;
+            this.labelColor.Text = "COLOR";
+            // 
+            // labelBoxNo
+            // 
+            this.labelBoxNo.AutoSize = true;
+            this.labelBoxNo.Location = new System.Drawing.Point(31, 465);
+            this.labelBoxNo.Name = "labelBoxNo";
+            this.labelBoxNo.Size = new System.Drawing.Size(55, 16);
+            this.labelBoxNo.TabIndex = 3;
+            this.labelBoxNo.Text = "BOX No";
+            // 
+            // labelQuantity
+            // 
+            this.labelQuantity.AutoSize = true;
+            this.labelQuantity.Location = new System.Drawing.Point(31, 648);
+            this.labelQuantity.Name = "labelQuantity";
+            this.labelQuantity.Size = new System.Drawing.Size(76, 16);
+            this.labelQuantity.TabIndex = 4;
+            this.labelQuantity.Text = "QUANTITY";
+            // 
+            // labelViewAndSearchWindow
+            // 
+            this.labelViewAndSearchWindow.AutoSize = true;
+            this.labelViewAndSearchWindow.Location = new System.Drawing.Point(34, 13);
+            this.labelViewAndSearchWindow.Name = "labelViewAndSearchWindow";
+            this.labelViewAndSearchWindow.Size = new System.Drawing.Size(197, 16);
+            this.labelViewAndSearchWindow.TabIndex = 1;
+            this.labelViewAndSearchWindow.Text = "VIEW  AND SEARCH WINDOW";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1648, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = " ";
+            // 
+            // buttonAdminAccess
+            // 
+            this.buttonAdminAccess.Location = new System.Drawing.Point(1736, 24);
+            this.buttonAdminAccess.Name = "buttonAdminAccess";
+            this.buttonAdminAccess.Size = new System.Drawing.Size(121, 23);
+            this.buttonAdminAccess.TabIndex = 3;
+            this.buttonAdminAccess.Text = "ADMIN ACCESS";
+            this.buttonAdminAccess.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxResults
+            // 
+            this.groupBoxResults.Controls.Add(this.checkBox1);
+            this.groupBoxResults.Controls.Add(this.pictureBox1);
+            this.groupBoxResults.Location = new System.Drawing.Point(306, 79);
+            this.groupBoxResults.Name = "groupBoxResults";
+            this.groupBoxResults.Size = new System.Drawing.Size(1600, 955);
+            this.groupBoxResults.TabIndex = 4;
+            this.groupBoxResults.TabStop = false;
+            this.groupBoxResults.Text = "Results";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(40, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(40, 22);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(18, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // FormViewSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1902, 1032);
+            this.Controls.Add(this.groupBoxResults);
+            this.Controls.Add(this.buttonAdminAccess);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelViewAndSearchWindow);
+            this.Controls.Add(this.groupBoxFilterElements);
+            this.Name = "FormViewSearch";
+            this.Text = "Form1";
+            this.groupBoxFilterElements.ResumeLayout(false);
+            this.groupBoxFilterElements.PerformLayout();
+            this.groupBoxResults.ResumeLayout(false);
+            this.groupBoxResults.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxFilterElements;
+        private System.Windows.Forms.Label labelQuantity;
+        private System.Windows.Forms.Label labelBoxNo;
+        private System.Windows.Forms.Label labelColor;
+        private System.Windows.Forms.Label labelSize;
+        private System.Windows.Forms.Label labelCategory;
+        private System.Windows.Forms.Label labelViewAndSearchWindow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonAdminAccess;
+        private System.Windows.Forms.GroupBox groupBoxResults;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
