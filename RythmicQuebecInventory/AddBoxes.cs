679627f9c0a11de74f241ba547f8d816262f15d2
fran chen <franfch@hotmail.com>
2022-07-29 11:51:23 -0400

    all parts

---
+++ RythmicQuebecInventory/AddBoxes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RythmicQuebecInventory
+{
+    public partial class AddBoxes : Form
+    {
+        public AddBoxes()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
